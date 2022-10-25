1c15718de6f66e8e9e03f15cfd644f98c4d05816
Aavash <aavash@gmail.com>
2022-10-25 08:46:13 +0545

    test for errors and changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,3 +15,6 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+
 private:
     Ui::MainWindow *ui;
     entry *Entry;
